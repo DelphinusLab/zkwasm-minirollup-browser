0bad28b31b19225a4bc8f383ffbe6cf9b522fc25
sinka <xgao@zoyoe.com>
2025-01-25 18:39:24 +1100

    move ConnectState to nontype export

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import AccountSliceReducer, * as AccountSlice from "./reduxstate.js";
 import { rpc } from "./connect.js";
 import { createStateSlice, RequestError, ConnectState, PropertiesState } from "./reduxconnect.js";
 
-export {AccountSlice, AccountSliceReducer, rpc, createStateSlice}
-export type {PropertiesState, ConnectState, RequestError}
+export {AccountSlice, AccountSliceReducer, rpc, createStateSlice, ConnectState}
+export type {PropertiesState, RequestError}
