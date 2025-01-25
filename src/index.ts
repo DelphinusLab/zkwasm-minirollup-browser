b8934c00edb3a3b5553973d1249593dd08878f74
sinka <xgao@zoyoe.com>
2025-01-25 18:12:58 +1100

    export more stuff

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import AccountSliceReducer, * as AccountSlice from "./reduxstate.js";
 import { rpc } from "./connect.js";
 import { createStateSlice, RequestError, ConnectState, PropertiesState } from "./reduxconnect.js";
 
-export {AccountSlice, AccountSliceReducer, rpc, createStateSlice}
+export {AccountSlice, AccountSliceReducer, rpc, createStateSlice, PropertiesState, ConnectState, RequestError}
